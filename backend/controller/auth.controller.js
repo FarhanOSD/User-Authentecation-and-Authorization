9e38af83f32ae35c5cba039507369b70d4d72def
FarhanOSD <ff3841362@gmail.com>
2024-12-21 11:39:27 +0600

    update

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,6 +1,6 @@
 import { User } from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
-import {sendVerificationEmail} from "../mailtrap/emails.js"
+import {sendVerificationEmail, sendWelcomeEmail} from "../mailtrap/emails.js"
 import {generateTokenAndSandCode} from "../util/generateTokenAndSandCode.js"
 
 export const singup = async (req, res) => {
@@ -54,8 +54,25 @@ export const verifyEmail = async (req, res) => {
   const { code } = req.body
   try {
     const user = await User.findOne({ 
-      
-     })
+      verificationToken: code,
+      verificationTokenExpiresAt: { $gt: Date.now() }
+    })
+    if (!user) {
+      throw new Error({ message: 'Invalid verification code' });
+    }
+    user.isVerified = true;
+    user.verificationToken = null;
+    user.verificationTokenExpiresAt = null;
+    await user.save();
+    await sendWelcomeEmail(user.email, user.name);
+    res.status(200).json({
+      success: true, message: 'Email verified successfully',
+      user: {
+      ...user, _doc,
+        password: null,
+    }
+     });
+    
   } catch (error) {
     
   }
